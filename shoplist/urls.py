4ef68a537eededf63f54a9aa2974878c647d584e
= <=>
2024-11-12 12:52:46 +0300

    add requirements

--- shoplist/urls.py
+++ shoplist/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('all/', views.ProductListing.as_view()),
+    path('/', views.ProductListing.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
