9116bd32533f98a61b15aa34bf3738a26965fd9d
= <=>
2024-11-12 13:21:06 +0300

    ready for deploy

--- shoplist/urls.py
+++ shoplist/urls.py
@@ -4,7 +4,8 @@ from products import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.ProductListing.as_view()),
+    path('', views.ProductListing.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
