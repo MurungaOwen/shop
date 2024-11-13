52d57ef1852b18cc25ee9ca146e4e254805b816c
= <=>
2024-11-13 22:51:12 +0300

    setp localhost in origins

--- shoplist/settings.py
+++ shoplist/settings.py
@@ -66,7 +66,10 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+	"http://localhost:5500",
+	"http://127.0.0.1:5500",
+]
 WSGI_APPLICATION = 'shoplist.wsgi.application'
 
 
